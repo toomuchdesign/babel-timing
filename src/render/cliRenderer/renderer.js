7864880706066370c542e7a98b1809cc65940ca5
Andrea Carraro <me@andreacarraro.it>
2019-05-12 18:10:17 +0200

    Use ansi-diff-stream only once in root renderer

--- src/render/cliRenderer/renderer.js
+++ src/render/cliRenderer/renderer.js
@@ -1,31 +1,41 @@
+var differ = require('ansi-diff-stream');
 const FileList = require('./FileList');
 const PluginList = require('./PluginList');
+const {enableKeyPressEvent} = require('./utils');
 
-function renderFileList(results, selected = 0) {
+function renderFileList(results, selected = 0, diff) {
   const output = new FileList({
     results,
     selected,
     onSelected: selected => {
       output.clear();
       output.stop();
-      renderPluginList(results, selected);
+      renderPluginList(results, selected, diff);
     },
+    diff,
   });
 }
 
-function renderPluginList(results, resultIndex) {
+function renderPluginList(results, resultIndex, diff) {
   const output = new PluginList({
     results: results[resultIndex],
     onBack: () => {
       output.clear();
       output.stop();
-      renderFileList(results, resultIndex);
+      renderFileList(results, resultIndex, diff);
     },
+    diff,
   });
 }
 
 function renderer(results = []) {
-  renderFileList(results);
+  enableKeyPressEvent();
+
+  // Init ansi-diff-stream
+  const diff = differ();
+  diff.pipe(process.stdout);
+
+  renderFileList(results, undefined, diff);
 }
 
 module.exports = renderer;
