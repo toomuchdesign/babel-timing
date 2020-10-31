226b78ae741286fca162766fc0ff3855ef3cc524
Andrea Carraro <me@andreacarraro.it>
2020-10-31 18:14:51 +0100

    TS migration III

---
+++ src/timersCollection.ts
@@ -0,0 +1,22 @@
+import Timer from './Timer';
+
+let timers = new Map<string, Timer>();
+
+export function getAll() {
+  return [...timers.entries()].map(entry => entry[1]);
+}
+
+export function clear() {
+  timers = new Map();
+}
+
+export function getTimer(file: string): Timer {
+  const timer = timers.get(file);
+  if (timer) {
+    return timer;
+  } else {
+    const newTimer = new Timer(file);
+    timers.set(file, newTimer);
+    return newTimer;
+  }
+}
